1b07d163b516e800a351799dbe21c3d6c8e87240
Charles <charlessmith2316@gmail.com>
2023-03-06 17:37:02 -0500

    lowrider

---
+++ Untitled-1.js
@@ -0,0 +1,53 @@
+//  1. What is your age group? 
+//            Option A : 18-27                
+//            Option B : 28-35
+//            Option C:  35+
+
+
+//  2.  Which of these best represent your terrain?
+//            Option A : Street City                
+//            Option B : Street Highway
+//            Option C:  Off-Road
+
+ 
+
+//  3.  Do you prefer electric or gas or hyrbid? 
+//           Option A : Electric 
+//           Option B :  Gas
+//           Option C:   Hybrid 
+
+
+
+//  4. Which one of these fits your stlye 
+//          Option A: Lowrider 
+//          Option B: Lifted Truck               
+//          Option C: Modified stationwagon
+
+
+//  5.   Which of these best represents where you park?
+//           Option A : City Street
+//           Option B : Garage
+//           Option C:  Outdoor parking
+
+
+
+
+
+
+
+
+
+// FEATURES :  mouseover to highlight    choices' outline box
+//          :  mouseover to highlight    prev, next, reset buttons 
+
+//          : image click events    
+//          : prev , next reset buttons 
+//          : choice form 
+//          : refresh button on page logo
+
+
+//   start with a list of objects then 
+//   based on choices , objects are removed from the final output 
+//   when one object remains, Remove A , C elements. Remove buttons, change b innerText to final car object .name change b img src to final car obj .img
+//   display final message ex : "This car would be perfect for you!" display confetti gif 
+
