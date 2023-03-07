527b7dfca8289a73d9774b86b18baf8238b4d04f
Freddy Perez <fred.perez15@gmail.com>
2023-03-07 23:49:21 +0200

    additional CSS

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const pullInfo = () => {
     fetch('http://localhost:3000/questions')
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
         data.forEach((assesmentImages) => {
             renderAssesmentPics(assesmentImages)
         })
     })
 }
 
+
 const renderAssesmentPics = (assesmentImages) => {
+    const choicesDiv = document.querySelector('.choices')
+    
+    const imgElement1 = document.createElement('img')
+    const imgElement2 = document.createElement('img')
+    const imgElement3 = document.createElement('img')
+
+    imgElement1.src = assesmentImages.image1
+    // imgElement2.src = assesmentImages.image2
+    // imgElement3.src = assesmentImages.image3
+    
+    choicesDiv.append(imgElement1,imgElement2,imgElement3)
    
 }
 
